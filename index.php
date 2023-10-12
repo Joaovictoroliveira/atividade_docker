281cbdee2bfc237659a439507ac6f2da7be9a815
João Victor <joao.gomes31@fatec.sp.gov.br>
2023-10-12 17:06:34 -0300

    css + arquivos templates + index

--- index.php
+++ index.php
@@ -1,11 +1,54 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Atividade docker</title>
-</head>
-<body>
-    <h1>Funcionando!</h1>
-</body>
-</html>
+<?php
+    include_once("templates/header.php");
+?>
+
+    <div class="container">
+        <?php if (isset($printMsg) && $printMsg != ""): ?>
+            <p id="msg"><?= $printMsg ?></p>
+        <?php endif; ?>
+    </div>
+
+    <h1 id="main-title">Países e capitais</h1>
+    <?php if (count($paises) > 0): ?>
+        <table class="table" id="paises-table">
+            <thead>
+                <table>
+                    <th scope="col">ID</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Capital</th>
+                    <th scope="col">Ações</th>
+                </table>
+            </thead>
+            <tbody>
+                <?php foreach ($paises as $pais): ?>
+                    <tr>
+                        <td scope="row" class="col-id"><?= $pais["id"] ?></td>
+                        <td scope="row"><?= $pais["nome"] ?></td>
+                        <td scope="row"><?= $pais["capital"] ?></td>
+                        <td class="actions">
+                            <a href="<?= $BASE_URL ?>show.php?id=<?= $pais["id"] ?>">
+                                <i class="bi bi-eye-fill"></i>
+                            </a>
+
+                            <a href="<?= $BASE_URL ?>edit.php?id=<?= $pais["id"] ?>">
+                                <i class="bi bi-pencil-fill"></i>
+                            </a>
+
+                            <form action="<?= $BASE_URL ?>config.process" class="delete-form" method="POST">
+                                <input type="hidden" name="type" value="delete">
+                                <input type="hidden" name="id" value="<?= $pais["id"] ?>">
+                                <button type="submit" class="delete-btn">
+                                    <i class="bi bi-archive-fill"></i>
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php else: ?>
+        <p id="empty-list-text">
+            Ainda não há países cadastrados,
+            <a href="<?= $BASE_URL ?>create.php">clique aqui para adicionar</a>
+        </p>
+    <?php endif; ?>
---
+++ templates/header.php
@@ -0,0 +1,33 @@
+<?php
+
+include_once("config/url.php");
+include_once("config/process.php");
+
+if (isset($_SESSION["msg"])) {
+    $printMsg = $_SESSION["msg"];
+    $_SESSION["msg"] = "";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Países e capitais</title>
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <!-- CSS -->
+    <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+          <div class="navbar-brand">
+            <a href="<?= $BASE_URL ?>index.php" id="home-link" class="nav-link active">Países</a>
+            <a href="<?= $BASE_URL ?>create.php" class="nav-link active">Adicionar país</a>
+          </div>
+        </nav>
+    </header>
